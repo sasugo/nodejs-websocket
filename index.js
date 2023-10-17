f17214787c9eccd60d5fdc4a88a64fe4ede4f1f9
Er. Gonzalo Unzueta <gunzueta@siwel.com.bo>
2023-10-17 16:35:20 -0400

    update

--- index.js
+++ index.js
@@ -3,26 +3,27 @@ const path = require("path");
 const WebSocket = require("ws");
 const app = express();
 
-// Use the public directory for static file requests
 app.use(express.static("public"));
 
-// Start our WS server at 3001
 const wss = new WebSocket.Server({ port: 3001 });
+const toHMI = {
+  signal1: "a",
+  signal2: "b",
+  signal3: "c",
+};
 
 wss.on("connection", (ws) => {
   console.log("A new client connected!");
   ws.on("message", (data) => {
     console.log(`Message from client: ${data}`);
 
-    // Modify the input and return the same.
     const parsed = JSON.parse(data);
     ws.send(
-      JSON.stringify({
-        ...parsed.data,
-        // Additional field set from the server using the from field.
-        // We'll see how this is set in the next section.
-        messageFromServer: `Hello tab id: ${parsed.data.from}`,
-      }),
+      JSON.stringify(
+        toHMI,
+        // ...parsed.data,
+        // messageFromServer: `Hello tab id: ${parsed.data.from}`,
+      ),
     );
   });
   ws.on("close", () => {
@@ -30,7 +31,6 @@ wss.on("connection", (ws) => {
   });
 });
 
-// Listen for requests for static pages at 3000
 const server = app.listen(3000, function () {
   console.log("The server is running on http://localhost:" + 3000);
 });
